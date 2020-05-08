068314fe5785189ce95c96b235650da2c44d75f3
superturboryan <forsyth.r@gmail.com>
2020-05-08 14:15:24 -0400

    Initial commit. Everything works

--- Volume Tester/ViewController.m
+++ Volume Tester/ViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "ViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController ()
+@interface ViewController () <AVAudioPlayerDelegate>
+
+@property (weak, nonatomic) IBOutlet MPVolumeView *mpVolumeView;
+@property (weak, nonatomic) IBOutlet UIButton *getVolumeButton;
+@property (weak, nonatomic) IBOutlet UILabel *currentVolumeLabel;
+
+@property (strong,nonatomic) AVAudioPlayer *player;
 
 @end
 
@@ -16,7 +24,56 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [AVAudioSession.sharedInstance setActive:YES error:nil];
+    
+    [AVAudioSession.sharedInstance setCategory:AVAudioSessionCategoryPlayback error:nil];
+    
+    self.mpVolumeView.backgroundColor = UIColor.clearColor;
+    
+    self.currentVolumeLabel.text = [NSString stringWithFormat:@"%f", [self currentSystemVolume]];
+    
+    [NSNotificationCenter.defaultCenter
+     addObserver:self
+     selector:@selector(updateVolumeLabel)
+     name:@"AVSystemController_SystemVolumeDidChangeNotification"
+     object:nil];
+}
+
+- (void)dealloc {
+    [NSNotificationCenter.defaultCenter removeObserver:self];
+}
+
+- (IBAction)tappedSetMaxVolume:(id)sender {
+    [(UISlider*)self.mpVolumeView.subviews[1] setValue:1];
+    [self performSelector:@selector(updateVolumeLabel) withObject:nil afterDelay:0.2];
+}
+
+- (IBAction)tappedSetMinVolume:(id)sender {
+    [(UISlider*)self.mpVolumeView.subviews[1] setValue:0];
+    [self performSelector:@selector(updateVolumeLabel) withObject:nil afterDelay:0.2];
+}
+
+- (IBAction)tappedPlaySound:(id)sender {
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"meow" ofType:@"mp3"];
+    
+    NSURL *soundFileURL = [NSURL fileURLWithPath:filePath];
+    self.player = [[AVAudioPlayer alloc] initWithContentsOfURL:soundFileURL
+                                                                   error:nil];
+    self.player.numberOfLoops = 0;
+    self.player.delegate = self;
+    
+    self.player.volume = [self currentSystemVolume];
+    
+    [self.player play];
+}
+
+-(void)updateVolumeLabel {
+    self.currentVolumeLabel.text = [NSString stringWithFormat:@"%f", [self currentSystemVolume]];
+}
+
+-(float)currentSystemVolume {
+    return AVAudioSession.sharedInstance.outputVolume;
 }
 
 
